43ad598e90d59bd96f0fda1c1b15b96914b4933f
LuoHaomin <LuoHaomin@mail.ustc.edu.cn>
2024-05-06 18:23:18 +0800

    origin

---
+++ src/main/java/cn/ustc/edu/course_selection_system/Database/PasswordChecher.java
@@ -0,0 +1,4 @@
+package cn.ustc.edu.course_selection_system.Database;
+
+public interface PasswordChecher {
+}
---
+++ src/main/java/cn/ustc/edu/course_selection_system/Service/Student.java
@@ -0,0 +1,4 @@
+package cn.ustc.edu.course_selection_system.Service;
+
+public interface Student {
+}
