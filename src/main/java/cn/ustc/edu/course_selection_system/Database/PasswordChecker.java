8ee62f9bea1af86f6d453d234ea46c3d6d26ebdc
LuoHaomin <LuoHaomin@mail.ustc.edu.cn>
2024-05-09 20:19:15 +0800

    change by luo (add basic class and interface)

--- src/main/java/cn/ustc/edu/course_selection_system/Database/PasswordChecker.java
+++ src/main/java/cn/ustc/edu/course_selection_system/Database/PasswordChecker.java
@@ -1,4 +1,5 @@
 package cn.ustc.edu.course_selection_system.Database;
 
 public interface PasswordChecker {
+    boolean isPasswordCorrect(int id,String password);
 }
--- src/main/java/cn/ustc/edu/course_selection_system/Service/StudentService.java
+++ src/main/java/cn/ustc/edu/course_selection_system/Service/StudentService.java
@@ -1,4 +1,18 @@
 package cn.ustc.edu.course_selection_system.Service;
 
-public interface StudentService {
+import cn.ustc.edu.course_selection_system.Bean.StudentInfo;
+import cn.ustc.edu.course_selection_system.Database.ID_Editor;
+
+import java.util.List;
+
+public class StudentService extends AbstractStudentService{
+    @Override
+    public StudentInfo GetID() {
+        return null;
+    }
+
+    @Override
+    public List<String> GetSchedule() {
+        return List.of();
+    }
 }
