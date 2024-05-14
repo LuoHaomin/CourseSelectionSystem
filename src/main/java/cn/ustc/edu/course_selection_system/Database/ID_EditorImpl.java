3cb150bd955f27b01cd792ce8760caf41062c294
LuoHaomin <LuoHaomin@mail.ustc.edu.cn>
2024-05-14 16:20:27 +0800

    change by luo (add basic class and interface)

---
+++ src/main/java/cn/ustc/edu/course_selection_system/Database/ID_EditorImpl.java
@@ -0,0 +1,10 @@
+package cn.ustc.edu.course_selection_system.Database;
+
+import cn.ustc.edu.course_selection_system.Bean.PersonInfo;
+
+public class ID_EditorImpl implements ID_Editor{
+    @Override
+    public PersonInfo GetID(int id) {
+        return null;
+    }
+}
--- src/main/java/cn/ustc/edu/course_selection_system/Service/StudentService.java
+++ src/main/java/cn/ustc/edu/course_selection_system/Service/StudentService.java
@@ -2,13 +2,15 @@ package cn.ustc.edu.course_selection_system.Service;
 
 import cn.ustc.edu.course_selection_system.Bean.StudentInfo;
 import cn.ustc.edu.course_selection_system.Database.ID_Editor;
+import cn.ustc.edu.course_selection_system.Database.ID_EditorImpl;
 
 import java.util.List;
 
 public class StudentService extends AbstractStudentService{
     @Override
     public StudentInfo GetID() {
-        return null;
+        ID_Editor idEditor = new ID_EditorImpl();
+        return (StudentInfo) idEditor.GetID(this.id);
     }
 
     @Override
