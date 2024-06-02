a7b083bd7e84b11cb5ebc098b43428098a62e03d
LISTOI <ustc_ls@mail.ustc.edu.cn>
2024-06-02 10:25:25 +0800

    Add_Some_New_Scene

--- src/main/java/cn/ustc/edu/course_selection_system/HelloApplication.java
+++ src/main/java/cn/ustc/edu/course_selection_system/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Loginscene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("教务选课系统");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/cn/ustc/edu/course_selection_system/View/NameandPasswordController.java
@@ -0,0 +1,17 @@
+package cn.ustc.edu.course_selection_system.View;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class NameandPasswordController {
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button login;
+}
