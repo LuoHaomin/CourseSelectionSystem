252e04692d0e00485b719e2cd159483325d2f5dd
LuoHaomin <LuoHaomin@mail.ustc.edu.cn>
2024-06-04 21:45:02 +0800

    change by luo (Implement of some function)

--- src/main/java/cn/ustc/edu/course_selection_system/Util/CourseTable.java
+++ src/main/java/cn/ustc/edu/course_selection_system/Util/CourseTable.java
@@ -5,7 +5,10 @@ import cn.ustc.edu.course_selection_system.Bean.CourseEntity;
 import java.util.List;
 
 public class CourseTable {
-    public CourseTable(List<CourseEntity> courseList){}
+    List<CourseEntity> list;
+    public CourseTable(List<CourseEntity> courseList){
+        list=courseList;
+    }
     public Boolean IsConflicted(CourseEntity courseEntity){
         return false;
     }
