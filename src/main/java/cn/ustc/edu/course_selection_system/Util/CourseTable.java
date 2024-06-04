fde20e32b21cd0d27faa14a5a0ee82e4aaa951e2
LuoHaomin <LuoHaomin@mail.ustc.edu.cn>
2024-06-04 22:04:04 +0800

    change by luo (Implement of some function)

--- src/main/java/cn/ustc/edu/course_selection_system/Util/CourseTable.java
+++ src/main/java/cn/ustc/edu/course_selection_system/Util/CourseTable.java
@@ -6,10 +6,20 @@ import java.util.List;
 
 public class CourseTable {
     List<CourseEntity> list;
+
+
     public CourseTable(List<CourseEntity> courseList){
         list=courseList;
     }
+
+    /**
+     * 判断输入是否与已有课程重叠
+     * @param courseEntity 待判断课程
+     * @return 是否重叠
+     */
     public Boolean IsConflicted(CourseEntity courseEntity){
         return false;
     }
+
+
 }
