0b8cabee0ce5d82c45c1305601ee43aae5146f89
ZichaoZou <1076909409@qq.com>
2024-06-05 21:13:27 +0800

    TEST

--- src/main/java/cn/ustc/edu/course_selection_system/Util/CourseTable.java
+++ src/main/java/cn/ustc/edu/course_selection_system/Util/CourseTable.java
@@ -12,12 +12,6 @@ public class CourseTable {
 
     public CourseTable (List<CourseEntity> courseList) { list = courseList; }
 
-
-    /**
-     * 判断输入是否与已有课程重叠
-     * @param courseEntity 待判断课程
-     * @return 是否重叠
-     */
     public Boolean IsConflicted(CourseEntity courseEntity){
         for (CourseEntity course:list) {
             if ( (course.getTime().equals(courseEntity.getTime())
@@ -28,7 +22,6 @@ public class CourseTable {
         return false;
     }
 
-   // public Boolean
 
     public List<CourseEntity> DayCourse (String day) {
         List<CourseEntity> dayCourse = new ArrayList<>();
