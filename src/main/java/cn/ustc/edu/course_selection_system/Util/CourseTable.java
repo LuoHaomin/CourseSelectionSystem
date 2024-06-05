1ba34f1fec7c73da1578197bdb551fd93497d4ac
ZichaoZou <1076909409@qq.com>
2024-06-05 19:56:48 +0800

    TEST

--- src/main/java/cn/ustc/edu/course_selection_system/Util/CourseTable.java
+++ src/main/java/cn/ustc/edu/course_selection_system/Util/CourseTable.java
@@ -2,15 +2,15 @@ package cn.ustc.edu.course_selection_system.Util;
 
 import cn.ustc.edu.course_selection_system.Bean.CourseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CourseTable {
+
     List<CourseEntity> list;
 
+    public CourseTable (List<CourseEntity> courseList) { list = courseList; }
 
-    public CourseTable(List<CourseEntity> courseList){
-        list=courseList;
-    }
 
     /**
      * 判断输入是否与已有课程重叠
@@ -18,7 +18,22 @@ public class CourseTable {
      * @return 是否重叠
      */
     public Boolean IsConflicted(CourseEntity courseEntity){
+        for (CourseEntity course:list) {
+            if (course.getTime() == courseEntity.getTime()) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public List<CourseEntity> DayCourse (String day) {
+        List<CourseEntity> dayCourse = new ArrayList<>();
+        for (CourseEntity course:list)  {
+            if (course.getTime()==day) {
+                dayCourse.add(course);
+            }
+        }
+        return dayCourse;
     }
 
 
