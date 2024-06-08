e2ff0ce12531ed283f8e29fa2d6986dc2361166f
mogoo7zn <znwangking@hotmail.com>
2024-06-08 16:41:43 +0800

    commit by mogu

--- src/main/java/cn/ustc/edu/course_selection_system/Database/DisciplinaryPlanData.java
+++ src/main/java/cn/ustc/edu/course_selection_system/Database/DisciplinaryPlanData.java
@@ -3,10 +3,17 @@ package cn.ustc.edu.course_selection_system.Database;
 import cn.ustc.edu.course_selection_system.Bean.MajorCourseEntity;
 import org.hibernate.SessionFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DisciplinaryPlanData {
     SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    /**
+     * Add a disciplinary plan
+     * @param major major
+     * @param courseNumber course number
+     */
     public void AddDisciplinaryPlan(String major, String courseNumber) {
         MajorCourseEntity majorCourseEntity = new MajorCourseEntity();
         majorCourseEntity.setMajor(major);
@@ -16,12 +23,35 @@ public class DisciplinaryPlanData {
         });
     }
 
+    /**
+     * Delete a disciplinary plan
+     * @param major major
+     * @param courseNumber course number
+     */
     public void DelDisciplinaryPlan(String major, String courseNumber) {
-
+        MajorCourseEntity majorCourseEntity = new MajorCourseEntity();
+        majorCourseEntity.setMajor(major);
+        majorCourseEntity.setCourseNumber(courseNumber);
+        sessionFactory.inTransaction(session -> {
+            session.remove(majorCourseEntity);
+        });
     }
 
+    /**
+     * Get the disciplinary plan
+     * @param major major
+     * @return List<MajorCourseEntity>
+     */
     public List<MajorCourseEntity> GetDisciplinaryPlan(String major) {
-        return null;
+        List<MajorCourseEntity> majorCourseList = new ArrayList<>();
+        sessionFactory.inSession(session -> {
+            List<MajorCourseEntity> _majorCourseList = session.createQuery("from MajorCourseEntity where major = :major",
+                            MajorCourseEntity.class)
+                    .setParameter("major", major)
+                    .getResultList();
+            majorCourseList.addAll(_majorCourseList);
+        });
+        return majorCourseList;
     }
 
 }
--- src/test/java/cn/ustc/edu/course_selection_system/tests/MajorCourseTest.java
+++ src/test/java/cn/ustc/edu/course_selection_system/tests/MajorCourseTest.java
@@ -1,7 +1,11 @@
 package cn.ustc.edu.course_selection_system.tests;
 
+import cn.ustc.edu.course_selection_system.Bean.MajorCourseEntity;
 import cn.ustc.edu.course_selection_system.Database.DisciplinaryPlanData;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MajorCourseTest {
     @Test
@@ -15,6 +19,16 @@ public class MajorCourseTest {
     @Test
     public void testGetDisciplinaryPlan() {
         DisciplinaryPlanData disciplinaryPlanData = new DisciplinaryPlanData();
-        System.out.println(disciplinaryPlanData.GetDisciplinaryPlan("软件工程"));
+        List<MajorCourseEntity> majorCourseList = new ArrayList<>();
+        majorCourseList = disciplinaryPlanData.GetDisciplinaryPlan("软件工程");
+        for (MajorCourseEntity majorCourseEntity : majorCourseList) {
+            System.out.println(majorCourseEntity.getCourseNumber());
+        }
+    }
+
+    @Test
+    public void testDelDisciplinaryPlan() {
+        DisciplinaryPlanData disciplinaryPlanData = new DisciplinaryPlanData();
+        disciplinaryPlanData.DelDisciplinaryPlan("软件工程", "C++");
     }
 }
