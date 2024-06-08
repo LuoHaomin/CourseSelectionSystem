ebd40e9245010c0f8a331db0d7b020fb01780a12
mogoo7zn <znwangking@hotmail.com>
2024-06-08 15:53:14 +0800

    commit by mogu

---
+++ src/test/java/cn/ustc/edu/course_selection_system/tests/MajorCourseTest.java
@@ -0,0 +1,14 @@
+package cn.ustc.edu.course_selection_system.tests;
+
+import cn.ustc.edu.course_selection_system.Database.DisciplinaryPlanData;
+import org.junit.jupiter.api.Test;
+
+public class MajorCourseTest {
+    @Test
+    public void testAddDisciplinaryPlan() {
+        DisciplinaryPlanData disciplinaryPlanData = new DisciplinaryPlanData();
+        disciplinaryPlanData.AddDisciplinaryPlan("软件工程", "C++");
+        disciplinaryPlanData.AddDisciplinaryPlan("软件工程", "Java");
+        disciplinaryPlanData.AddDisciplinaryPlan("软件工程", "Python");
+    }
+}
