f76e151b93735fca9da66c8ca85798642a66c2ed
LISTOI <ustc_ls@mail.ustc.edu.cn>
2024-06-10 15:46:16 +0800

    add some controller

--- src/main/java/cn/ustc/edu/course_selection_system/View/StudentCourseListController.java
+++ src/main/java/cn/ustc/edu/course_selection_system/View/StudentCourseListController.java
@@ -105,7 +105,7 @@ public class StudentCourseListController {
     @FXML
     public void HandleCourseList(ActionEvent event) throws IOException
     {}
-    class tableline
+    public class tableline
     {
         String Monday;
         String Tuesday;
@@ -124,9 +124,7 @@ public class StudentCourseListController {
             this.Saturday=Saturday;
             this.Sunday=Sunday;
         }
-        public String getMonday() {
-            return Monday;
-        }
+        public String getMonday() {return Monday;}
         public void setMonday(String Monday) {
             this.Monday = Monday;
         }
@@ -203,9 +201,9 @@ public class StudentCourseListController {
         for(int i=1;i<=13;i++)
         {
             List<String> timecourse=courseTable.TimeCourse(time[i],"1");
-            table.getItems().add(new tableline(timecourse.get(0),timecourse.get(1),timecourse.get(2), timecourse.get(3), timecourse.get(4), timecourse.get(5), timecourse.get(6)));
+            list.add(new tableline(timecourse.get(0),timecourse.get(1),timecourse.get(2), timecourse.get(3), timecourse.get(4), timecourse.get(5), timecourse.get(6)));
         }
-        table=new TableView<>(list);
+        table.setItems(list);
     }
     @FXML
     private TextField textfield;
@@ -225,8 +223,8 @@ public class StudentCourseListController {
         for(int i=1;i<=13;i++)
         {
             List<String> timecourse=courseTable.TimeCourse(time[i],Week);
-            table.getItems().add(new tableline(timecourse.get(0),timecourse.get(1),timecourse.get(2), timecourse.get(3), timecourse.get(4), timecourse.get(5), timecourse.get(6)));
+            list.add(new tableline(timecourse.get(0),timecourse.get(1),timecourse.get(2), timecourse.get(3), timecourse.get(4), timecourse.get(5), timecourse.get(6)));
         }
-        table=new TableView<>(list);
+        table.setItems(list);
     }
 }
