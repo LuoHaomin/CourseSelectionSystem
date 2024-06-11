73f3f38786cd515110444039ff1e89e742e1a563
mogoo7zn <znwangking@hotmail.com>
2024-06-11 22:54:43 +0800

    commit by mogu, upload edition

--- src/main/java/cn/ustc/edu/course_selection_system/HelloApplication.java
+++ src/main/java/cn/ustc/edu/course_selection_system/HelloApplication.java
@@ -8,6 +8,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    /**
+     * 启动程序
+     * @param stage 启动
+     * @throws IOException IO异常
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Loginscene.fxml"));
